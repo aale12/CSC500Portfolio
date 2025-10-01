3149e39e5a9c78d0cf4b4853f15d949208ac11f3
Anthony <leanthony104@yahoo.com>
2025-10-01 05:00:16 -0500

    Initial commit

---
+++ CSC500CTA4.py
@@ -0,0 +1,35 @@
+class ItemToPurchase:
+    def __init__(self, item_name="none", item_price=0.0, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+
+    def print_item_cost(self):
+        total_cost = self.item_price * self.item_quantity
+        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price:.2f} = ${total_cost:.2f}")
+
+    def get_total_cost(self):
+        return self.item_price * self.item_quantity
+
+
+if __name__ == "__main__":
+    # Item 1
+    item1_name = input("Enter the name of item 1:\n")
+    item1_price = float(input("Enter the price of item 1:\n"))
+    item1_quantity = int(input("Enter the quantity of item 1:\n"))
+
+    # Item 2
+    item2_name = input("\nEnter the name of item 2:\n")
+    item2_price = float(input("Enter the price of item 2:\n"))
+    item2_quantity = int(input("Enter the quantity of item 2:\n"))
+
+    # Create objects
+    item1 = ItemToPurchase(item1_name, item1_price, item1_quantity)
+    item2 = ItemToPurchase(item2_name, item2_price, item2_quantity)
+
+    # Print receipt
+    print("\nTOTAL COST")
+    item1.print_item_cost()
+    item2.print_item_cost()
+    total = item1.get_total_cost() + item2.get_total_cost()
+    print(f"\nTotal: ${total:.2f}")
